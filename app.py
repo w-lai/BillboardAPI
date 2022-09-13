04e6e4a7e32d92b124f5d9e4f7d968fad1b7e493
Warren Lai <90171336+w-lai@users.noreply.github.com>
2022-09-13 08:19:29 -0700

    Update app.py

--- app.py
+++ app.py
@@ -33,12 +33,12 @@ api.add_resource(SongList, '/songs/<string:songnumber>')
 
 def main():
   # set up the selenium parser to use chrome
-  chrome_options = webdriver.ChromeOptions()
+  chrome_options = webdriver.Options()
   chrome_options.add_argument("--headless")
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--disable-dev-shm-usage')
   chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
   # access the billboard site dynamically
   driver.get("https://www.billboard.com/charts/hot-100/")
   # optain and parse the lines of the table
