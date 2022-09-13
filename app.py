428d24e86c3bbdc67fa5210ac653880305a0dd66
Warren Lai <90171336+w-lai@users.noreply.github.com>
2022-09-13 08:46:27 -0700

    Update app.py

--- app.py
+++ app.py
@@ -84,7 +84,8 @@ def main():
 
 if __name__ == "__main__":
     main()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    myPort = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=myPort, debug=True)
 
 
     
