a669b8b5181fb1527672ee00d18e9ca79d7144c4
Warren Lai <90171336+w-lai@users.noreply.github.com>
2022-09-13 08:26:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def main():
   chrome_options.add_argument('--no-sandbox')
   chrome_options.add_argument('--disable-dev-shm-usage')
   chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-  driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+  driver = webdriver.Chrome(service=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
   # access the billboard site dynamically
   driver.get("https://www.billboard.com/charts/hot-100/")
   # optain and parse the lines of the table
